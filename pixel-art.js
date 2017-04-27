7453ef7d121fe39ce359b3c57844808d00339138
Abby Starnes <abbymstarnes@gmail.com>
2017-04-27 13:05:04 -0500

    basic functionality

--- pixel-art.js
+++ pixel-art.js
@@ -4,13 +4,16 @@
 // create grid of empty pixels
 document.addEventListener("DOMContentLoaded", function() {
 
+   // create grid
+   let pixelGrid = document.getElementById('pixelGrid');
+
    function createGrid(size) {
-      let pixelGrid = document.getElementById('pixelGrid');
-      console.log(pixelGrid, 'a');
+      // create row
       for (let x = 0; x < size; x++) {
          let row = document.createElement('div');
          row.className = 'row';
          pixelGrid.appendChild(row);
+         // create cells
          for (let y = 0; y < size; y++) {
             let pixel = document.createElement('div');
             pixel.className = 'pixel';
@@ -19,15 +22,43 @@ document.addEventListener("DOMContentLoaded", function() {
       }
    }
 
-   createGrid(2);
+   createGrid(9);
+
+   // get all pixels
+   let pixels = document.getElementsByClassName('pixel');
+   // get all palette colors
+   let paletteColors = document.getElementsByClassName('palette-color');
+   console.log(paletteColors);
+   // change color function
+   // set brush color to default
+   var brushColor = ' red';
+
+   // brush color indicator
+   var currentColorIndicator = document.getElementById('currentColor');
+   // set brush color to new color
+   var changeBrushColor = function(event) {
+      var newColor = this.className.split(" ")[1];
+      brushColor = ` ${newColor}`;
+      currentColorIndicator.className = `current-color ${newColor}`;
+
+   }
+   // add event listener to all palette colors, run change color on click
+   for (let p = 0; p < paletteColors.length; p++) {
+      paletteColors[p].addEventListener('click', changeBrushColor);
+      //console.log(brushColor);
+   }
+
+   var colorPixel = function(event) {
+      this.className += brushColor;
+   }
+
+   // add event listener to all pixels, run change color on click
+   for (let z = 0; z < pixels.length; z++) {
+      pixels[z].addEventListener('click', colorPixel);
+   }
+
 
 });
 
-// var imgLog = function() {
-//    console.log("You moused over Mega Man!");
-// }
-//
-// document.addEventListener("DOMContentLoaded", function() {
-//    var img = document.querySelector('img');
-//    img.addEventListener('mouseover', imgLog);
-// });
+
+// if you click a color in palette, add set color equal to that color
